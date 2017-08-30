3ea71ff845bed827d31b7f4a14c0aaeb4377d1f1
Aaron Polley <aarondavidpolley@gmail.com>
2017-08-30 17:28:47 +1000

    Logging Added

--- smbWATCH.sh
+++ smbWATCH.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
+################################################################################
+# Author: 	Aaron Polley                                                       #
+# Date:		30/08/2017                                                         #
+# Version:	0.04                                                               #
+# Purpose:  Scripting for monitoring and unmounting SMB drives                 #
+#           Should be triggered by LaunchAgent						           #
+################################################################################
+
+#---Variables and such---#
+script_version="0.04"
+user_id=`id -u`
+user_name=`id -un $user_id`
+home_dir=`dscl . read /Users/"$user_name" NFSHomeDirectory | awk '{print $2}'`
+log_file="$home_dir/Library/Logs/smbWATCH.log"
+os_vers=`sw_vers -productVersion | awk -F "." '{print $2}'`
+
+#---Redirect output to log---#
+exec >> $log_file 2>&1
+
 #osascript -e "tell Application \"System Events\" to display alert\
 # \"Incorrect server connection detected, removing\""
+
+echo "*************************************************************************"
+echo `date "+%a %b %d %H:%M:%S"` " - smbWATCH beginning v${script_version}"
+echo `date "+%a %b %d %H:%M:%S"` "     - User:              $user_name"
+echo `date "+%a %b %d %H:%M:%S"` "     - User ID:           $user_id"
+echo `date "+%a %b %d %H:%M:%S"` "     - Home Dir:          $home_dir"
+echo `date "+%a %b %d %H:%M:%S"` "     - OS Vers:           10.${os_vers}"
+
+echo `date "+%a %b %d %H:%M:%S"` " - Unmounting network volumes..."
 
 ShareName="DPN"
 
@@ -19,19 +47,23 @@ smbSharesDomain=`mount | grep "smbfs" | grep "$ShareDomain" | awk '{print $1}'`
 #echo "Unmounting the following SMB Shares"
 
 for a in $smbSharesName ; do
-            echo "$a"
+            echo "Un-mounting $a"
             /sbin/umount -f "$a"
 done
 
 for b in $smbSharesIP ; do
-            echo "$b"
+            echo "Un-mounting $b"
             /sbin/umount -f "$b"
 done
 
 for c in $smbSharesDomain ; do
-            echo "$c"
+            echo "Un-mounting $c"
             /sbin/umount -f "$c"
 done
 
+
+echo `date "+%a %b %d %H:%M:%S"` " - Complete..."
+
+echo "*************************************************************************"
 
 exit 0
